c669956e4321a50a880f6e4d038fadb515514418
jhpiedrahitao <jhpiedrahitao@unal.edu.co>
2015-11-18 12:48:23 -0500

    spi

--- SPI_SD/dec_address.v
+++ SPI_SD/dec_address.v
@@ -5,7 +5,7 @@
 // 
 // Create Date:    11:52:12 11/04/2015 
 // Design Name: 
-// Module Name:    Top-SPI 
+// Module Name:    dec_address
 // Project Name: 
 // Target Devices: 
 // Tool versions: 
@@ -18,26 +18,20 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module top_spi(
-	imput select,
-	input [15:0] address,
-	input [15:0] data_in,
+module dec_address(
+	imput start,
+	input [15:0] address_in,
 	input clk,
-	input read,
-	imput write,
 	imput reset,
-	output data_out
+	output [15:0] address_out
 );
 
-reg data_out;
-reg data_in;
-reg address;
+reg address_in;
+reg address_out;
 always @(posedge clk) begin
 	if(reset) begin 
-		data_out <= 4'h0000;
-		data_in <= 4'h0000;
-		addres <= 4´h0000;
-		select <= 1'b0;
+		address_in <= 4'h0000;
+		address_out <= 4'h0000;
 	end 
 	elese begin
 		if(select) begin
