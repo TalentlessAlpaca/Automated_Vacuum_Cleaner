07118ebb8814225fa27024b44459b826dc2fc3ce
salsantacruzan <salsantacruzan@unal.edu.co>
2015-11-19 17:20:21 -0500

    actualización+testbenches-Timer

--- Timer_Counter/clk_counter.v
+++ Timer_Counter/clk_counter.v
@@ -1,18 +1,51 @@
-begin module clk_counter(
+module clk_counter(
     input clk,
-	input en,
-	input rst,
-   	output reg [31:0] Count,
-	);
+    input en,
+    input rst,
+    output reg [31:0] count
+    );
 	 
-	 reg [31:0] Count;
-	 	 	 
-	 always@(posedge clk) begin
+always@(posedge clk) begin
 		if(rst) begin
-			Count <= 31'b0;
+			count <= 31'b0;
 		end
 		else if (en) begin
-			Count = Count+1;
+			count = count+1;
 		end
-	 
-end module
+	 end
+
+endmodule
+
+//test bench
+module counter_tst;
+
+	// Inputs
+	reg clk;
+	reg en;
+	reg rst;
+
+	// Outputs
+	wire [31:0] counter;
+
+	// Instantiate the Unit Under Test (UUT)
+	clk_counter uut (
+		.clk(clk), 
+		.en(en), 
+		.rst(rst), 
+		.count(counter)
+	);
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		en = 0;
+		rst = 1;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+      rst = 0;
+		// Add stimulus here
+		#15 en = 1;
+	end
+   always #5 clk = ~clk;
+endmodule
--- Timer_Counter/clk_generator.v
+++ Timer_Counter/clk_generator.v
@@ -1,14 +1,15 @@
-begin module clk_generator(
+module clk_generator(
     input clk,
-	input en,
+    input en,
     input [31:0] limit,
-	input [31:0] count,
-	output reg clk_0,
-	);
-	 
-	 reg [31:0] nd_Count;
+    input [31:0] count,
+    output reg clk_0
+    );
+
+	 reg [31:0] ndCount;
 	 
 	 initial clk_0 = 1'b0;
+	 initial ndCount = 32'h00000000;
 	 
 	 always@(negedge clk) begin
 		if(en) begin
@@ -26,5 +27,57 @@ begin module clk_generator(
 			clk_0 <= 1'b0;
 		end
 	 end
-	 
-end module
+
+endmodule
+
+//test bench
+
+module gen_clk_tst;
+
+	// Inputs
+	reg clk;
+	reg rst;
+	reg en_cnt;
+	reg en;
+	reg [31:0] limit;
+	wire [31:0] count;
+
+	// Outputs
+	wire clk_0;
+
+	// Instantiate the Unit Under Test (UUT)
+	clk_generator uut (
+		.clk(clk), 
+		.en(en_cnt), 
+		.limit(limit), 
+		.count(count), 
+		.clk_0(clk_0)
+	);
+	
+	clk_counter counter(
+		.clk(clk), 
+		.en(en), 
+		.rst(rst), 
+		.count(count)
+	);
+	
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		rst = 1;
+		en = 0;
+		en_cnt = 0;
+		limit = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+      rst = 0;
+		// Add stimulus here
+		limit = 32'h000000FF;
+		#13;
+		en_cnt = 1;
+		#100;
+		en = 1;
+	end
+   always #5 clk = ~clk;
+endmodule
