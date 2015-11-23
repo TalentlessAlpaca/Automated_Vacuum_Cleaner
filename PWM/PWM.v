c992fbe6379b2c1bddd6bb3bd39e552788385ba1
AndOrtegon <afortegonv@unal.edu.co>
2015-11-22 20:04:13 -0500

    Update PWM.v

--- PWM/PWM.v
+++ PWM/PWM.v
@@ -1,28 +1,23 @@
-module PWM #(parameter LENGHT = 10) (
+module PWM #(parameter L=10 ) (
 	input clk,
 	input rst_n,
-	input [LENGHT - 1 : 0] number,
+	input [L-1 : 0] number,
 	output pwm
 	);
-   
-	reg [LENGHT - 1: 0] counter ;
-	reg [LENGHT - 1: 0] counter_temp ;
-
-		always @ ( posedge ) begin
+	
+	reg [L-1:0] counter ;
+	
+	always @ ( posedge clk ) begin
 		if ( rst_n ) begin
+		        counter <= counter + 1'b1 ;
 			if ( number > counter )
 				pwm <= 1'b1 ;
 			else 
 				pwm <= 1'b0 ;
-		end
-		else begin
-			counter_temp 	<= 1'b0 ;
-			pwm     		<= 1'b1 ;
+		end else begin
+			counter 	<= 1'b0 ;
+			pwm     	<= 1'b1 ;
 		end
 	end
 	
-	always @ ( * ) begin
-		counter = counter_temp ;
-		counter = counter + 1'b1 ;
-	end
 endmodule
