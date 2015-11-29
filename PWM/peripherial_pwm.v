e993f749a224ee6402e62760e12fe35b67186c63
AndOrtegon <afortegonv@unal.edu.co>
2015-11-28 19:44:19 -0500

    Update peripherial_pwm.v

--- PWM/peripherial_pwm.v
+++ PWM/peripherial_pwm.v
@@ -1,14 +1,14 @@
-module peripheral_pwm (clk , rst , d_in , cs , addr , rd , wr, d_out );
-  
-	input clk;
-	input rst;
-	input [15:0]d_in;
-	input cs;
-	input [3:0]	addr; // 4 LSB from j1_io_addr
-	input rd;
-	input wr;
-	output reg [2:0] d_out;
-
+module peripheral_pwm (
+	input clk, 
+	input rst, 
+	input [15:0] d_in, 
+	input cs,
+	input [3:0]addr, // 4 LSB from j1_io_addr
+	input rd,
+	input wr, 
+	output [2:0] pwm ,
+	output reg [2:0] d_out
+	);
 //------------------------------------ regs and wires-------------------------------
 
 	reg [5:0] 	s; 	//selector mux_4  and write registers
@@ -23,20 +23,13 @@ module peripheral_pwm (clk , rst , d_in , cs , addr , rd , wr, d_out );
 
 	always @(*) begin//------address_decoder------------------------------
 		case (addr)
-			4'h0:	
-				begin s = (cs && wr) ? 6'b00001 : 6'b00000 ;end //rst_n
-			4'h2:
-				begin s = (cs && wr) ? 6'b00010 : 6'b00000 ;end //number_1	
-			4'h4:
-				begin s = (cs && wr) ? 6'b00100 : 6'b00000 ;end //number_2
-			4'h6:
-				begin s = (cs && wr) ? 6'b01000 : 6'b00000 ;end //number_3
+			4'h0:	begin s = (cs && wr) ? 5'b00001 : 5'b00000 ;end //rst_n
+			4'h2:	begin s = (cs && wr) ? 5'b00010 : 5'b00000 ;end //number_1	
+			4'h4:	begin s = (cs && wr) ? 5'b00100 : 5'b00000 ;end //number_2
+			4'h6:	begin s = (cs && wr) ? 5'b01000 : 5'b00000 ;end //number_3
 				
-			4'h8:
-				begin s = (cs && rd) ? 6'b10000 : 6'b00000 ;end //pwm
-			
-			default:
-				begin s = 6'b00 ; end
+			4'h8:	begin s = (cs && rd) ? 5'b10000 : 5'b00000 ;end //pwm
+			default:begin s = 5'b00000 ; end
 		endcase
 	end//------------------address_decoder--------------------------------
 
