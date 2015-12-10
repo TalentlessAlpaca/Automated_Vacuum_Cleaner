63beb79548c8fe4584a47a8ef82c0e53e0095801
jhpiedrahitao <jhpiedrahitao@unal.edu.co>
2015-12-10 04:49:16 -0500

    se agregan modulos a j1soc.v y a mapa de memoria

--- j1_soc/firmware/Hello_World/hw_defs.fs
+++ j1_soc/firmware/Hello_World/hw_defs.fs
@@ -53,6 +53,62 @@ h# 7606 constant crc_16_done
 h# 7608 constant crc_16_data_out
 
 
+\ pwm:
+
+h# 6000 constant rst_n		
+h# 6002 constant number_1
+h# 6004 constant number_2
+h# 6006 constant number_3
+
+\ lcd:
+
+h# 6100 constant en
+h# 6101 constant reset
+h# 6102 constant set
+h# 6103 constant off
+h# 6104 constant on
+h# 6105 constant entry_mode	
+h# 6106 constant cursor
+h# 6107 constant w_char
+h# 6108 constant cursor_pos
+h# 6109 constant ascii_char
+
+\ posicion:
+
+h# 6200 constant en_r
+h# 6201 constant busy_r
+h# 6202 constant w_ram_r
+h# 6203 constant w_ram_ref_r
+h# 6204 constant w_ram_address_r
+h# 6205 constant w_ram_data_up_r 
+h# 6206 constant w_ram_data_low_r
+h# 6207 constant vX_up_r
+h# 6208 constant vX_low_r
+h# 6209 constant vY_up_r
+h# 6210 constant vY_low_r
+h# 6211 constant pX_up_r
+h# 6212 constant pX_low_r	
+h# 6213 constant pY_up_r
+h# 6214 constant pY_low_r
+h# 6215 constant theta_up_r
+h# 6216 constant theta_low_r
+
+
+
+\ timmer:
+
+h# 6300 constant  ena1
+h# 6301 constant  ena2
+h# 6302 constant  l1
+h# 6303 constant salidainterr
+h# 6304 constant counter
+
+
+\ distancia:
+
+h# 6400 constant Done
+h# 6401 constant Value
+
 \ constantes 
 
 \ constantes memoria 
--- j1_soc/hdl/j1soc.v
+++ j1_soc/hdl/j1soc.v
@@ -12,11 +12,28 @@ module j1soc#(
    output uart_tx;
    output ledout;
 
+   //spi
 	input mosi;
 	output miso;
 	output sck;
 	output ss;
-
+	
+	//pwm
+	output pwm [2,0];
+	
+	//LCD
+	
+	output rs;
+	output e;
+	output data;
+	
+	//posicion
+	
+	output SCL;
+	inout SDA;
+	
+	assign SDA = (SDA_oen) ? SDA_out : 1'bz;
+	assign SDA_in = SDA;
 
 //------------------------------------ regs and wires-------------------------------
 
@@ -28,7 +45,7 @@ module j1soc#(
 
 
  
-   reg [1:7]cs;  // CHIP-SELECT //cantidad de modulos 
+   reg [1:12]cs;  		// CHIP-SELECT //cantidad de modulos 
 
    wire			[15:0] mult_dout;  
    wire			[15:0] div_dout;
@@ -37,6 +54,11 @@ module j1soc#(
    wire			[15:0] spi_master_dout;
    wire			[15:0] crc_7_dout;
    wire			[15:0] crc_16_dout;
+   wire 		[15:0] pwm_dout;
+   wire 		[15:0] lcd_dout;
+   wire 		[15:0] posicion_dout;
+   wire 		[15:0] timmer_dout;
+   wire			[15:0] distancia_dout;  
  
 
 //------------------------------------ regs and wires-------------------------------
@@ -44,11 +66,11 @@ module j1soc#(
 
   j1 #(bootram_file)  cpu0(sys_clk_i, sys_rst_i, j1_io_din, j1_io_rd, j1_io_wr, j1_io_addr, j1_io_dout);
 
-  peripheral_mult  per_m (.clk(sys_clk_i), .rst(sys_rst_i), .d_in(j1_io_dout), .cs(cs[1]), .addr(j1_io_addr[3:0]), .rd(j1_io_rd), .wr(j1_io_wr), .d_out(mult_dout) );
+  peripheral_mult  per_m (.clk(sys_clk_i), .rst(sys_rst_i), .d_in(j1_io_din), .cs(cs[1]), .addr(j1_io_addr[3:0]), .rd(j1_io_rd), .wr(j1_io_wr), .d_out(mult_dout) );
 
-  peripheral_div  per_d (.clk(sys_clk_i), .rst(sys_rst_i), .d_in(j1_io_dout), .cs(cs[2]), .addr(j1_io_addr[3:0]), .rd(j1_io_rd), .wr(j1_io_wr), .d_out(div_dout));
+  peripheral_div  per_d (.clk(sys_clk_i), .rst(sys_rst_i), .d_in(j1_io_din), .cs(cs[2]), .addr(j1_io_addr[3:0]), .rd(j1_io_rd), .wr(j1_io_wr), .d_out(div_dout));
 
-  peripheral_uart  per_u (.clk(sys_clk_i), .rst(sys_rst_i), .d_in(j1_io_dout), .cs(cs[3]), .addr(j1_io_addr[3:0]), .rd(j1_io_rd), .wr(j1_io_wr), .d_out(uart_dout), .uart_tx(uart_tx), .ledout(ledout));
+  peripheral_uart  per_u (.clk(sys_clk_i), .rst(sys_rst_i), .d_in(j1_io_din), .cs(cs[3]), .addr(j1_io_addr[3:0]), .rd(j1_io_rd), .wr(j1_io_wr), .d_out(uart_dout), .uart_tx(uart_tx), .ledout(ledout));
 
 
   dpRAM_interface dpRm(.clk(sys_clk_i), .d_in(j1_io_dout), .cs(cs[4]), .addr(j1_io_addr[7:0]), .rd(j1_io_rd), .wr(j1_io_wr), .d_out(dp_ram_dout));
@@ -56,25 +78,42 @@ module j1soc#(
 
   peripheral_spi_master  per_spi (.clk(sys_clk_i), .rst(sys_rst_i), .d_in(j1_io_dout), .cs(cs[5]), .addr(j1_io_addr[3:0]), .rd(j1_io_rd), .wr(j1_io_wr), .d_out(spi_master_dout) , .mosi(mosi) ,.miso(miso), .sck(sck), .ss(ss) ); //se instancia el peripheral de spi
 
- peripheral_crc_7  per_crc_7 (.clk(sys_clk_i), .rst(sys_rst_i), .d_in(j1_io_dout), .cs(cs[6]), .addr(j1_io_addr[3:0]), .rd(j1_io_rd), .wr(j1_io_wr), .d_out(crc_7_dout) ); //se instancia el peripheral de crc_7
+  peripheral_crc_7  per_crc_7 (.clk(sys_clk_i), .rst(sys_rst_i), .d_in(j1_io_dout), .cs(cs[6]), .addr(j1_io_addr[3:0]), .rd(j1_io_rd), .wr(j1_io_wr), .d_out(crc_7_dout) ); //se instancia el peripheral de crc_7
 
- peripheral_crc_16  per_crc_16 (.clk(sys_clk_i), .rst(sys_rst_i), .d_in(j1_io_dout), .cs(cs[7]), .addr(j1_io_addr[3:0]), .rd(j1_io_rd), .wr(j1_io_wr), .d_out(crc_16_dout) ); //se instancia el peripheral de crc_16
+  peripheral_crc_16  per_crc_16 (.clk(sys_clk_i), .rst(sys_rst_i), .d_in(j1_io_dout), .cs(cs[7]), .addr(j1_io_addr[3:0]), .rd(j1_io_rd), .wr(j1_io_wr), .d_out(crc_16_dout) ); //se instancia el peripheral de crc_16
 
+  peripheral_pwm per_pwm (.clk(sys_clk_i),.rst(sys_rst_i),.d_in(j1_io_dout),.d_out(pwm_out),.cs(cs[8]),.addr(j1_io_addr[3:0]),.rd(j1_io_rd),.wr(j1_io_wr),.pwm(pwm));
 
+  LCD_Peripheral  per_lcd (.clk(sys_clk_i), .rst(sys_rst_i), .d_in(j1_io_dout), .cs(cs[9]), .addr(j1_io_addr[3:0]), .rd(j1_io_rd), .wr(j1_io_wr), .d_out(lcd_out), .rs(lcd_rs), .e(lcd_e), .data(lcd_data), .int_cnt(int_cnt), .en_cnt(en_cnt), .limit_cnt(limit_cnt) ); 
+
+  position_peripheral (.clk(sys_clk_i), .rst(sys_rst_i), .d_in(j1_io_dout), .cs(cs[10]), .addr(j1_io_addr[3:0]), .rd(j1_io_rd), .wr(j1_io_wr), .d_out(posicion_out), .clk_freq(clk_freq), .counter_rst(counter_rst), .counter_en (counter_en), . int_en(int_en), .int_limit(int_limit), .SCL(SCL), .SDA_out(SDA_out), .SDA_oen(SDA_oen), .counter_count(counter_count), .clk_frame(clk_frame), .int_o(int_o), .SDA_in(SDA_in); 
+  
+  peripheral_timmer (.clk(sys_clk_i), .rst(sys_rst_i), .d_in(j1_io_dout), .cs(cs[11]), .addr(j1_io_addr[3:0]), .rd(j1_io_rd), .wr(j1_io_wr),.d_out( timmer_out ));
+  
+  peripheral_distancia (.clk(sys_clk_i), .rst(sys_rst_i), .d_in(j1_io_dout), .cs(cs[12]), .addr(j1_io_addr[3:0]), .rd(j1_io_rd), .wr(j1_io_wr),.d_out( distancia_out ));
+  
+  
   // ============== Chip_Select (Addres decoder) ========================  // se hace con los 8 bits mas significativos de j1_io_addr
   always @*
   begin
-      case (j1_io_addr[15:8])	// direcciones - chip_select
-        8'h67: cs= 7'b1000000; 		//mult
-        8'h68: cs= 7'b0100000;		//div
-        8'h69: cs= 7'b0010000;		//uart
-        8'h70: cs= 7'b0001000;		//dp_ram
-        8'h72: cs= 7'b0000100;		//spi
-	8'h74: cs= 7'b0000010;          //crc_7
-	8'h76: cs= 7'b0000001;		//crc_16
-
-     
-        default: cs= 7'b0000000;
+      case (j1_io_addr[15:8])				// direcciones - chip_select
+        8'h67: cs= 12'b100000000000; 		//mult
+        8'h68: cs= 12'b010000000000;		//div
+        8'h69: cs= 12'b001000000000;		//uart
+        8'h70: cs= 12'b000100000000;		//dp_ram
+        8'h72: cs= 12'b000010000000;		//spi
+	8'h74: cs= 12'b000001000000;     	//crc_7
+	8'h76: cs= 12'b000000100000;		//crc_16
+        8'h60: cs= 12'b000000010000;		//pwm
+        8'h61: cs= 12'b000000001000;		//lcd
+        8'h62: cs= 12'b000000000100;		//posicion
+        8'h63: cs= 12'b000000000010;		//timmer
+	8'h64: cs= 12'b000000000001;     	//distancia
+		
+		
+		
+		
+        default: cs= 12'b000000000000;
       endcase
   end
   // ============== Chip_Select (Addres decoder) ========================  //
@@ -86,15 +125,21 @@ module j1soc#(
   always @*
   begin
       case (cs)
-        7'b1000000: j1_io_din = mult_dout; 
-        7'b0100000: j1_io_din = div_dout;
-        7'b0010000: j1_io_din = uart_dout; 
-        7'b0001000: j1_io_din = dp_ram_dout; 
-        7'b0000100: j1_io_din = spi_master_dout; 
-	7'b0000010: j1_io_din = crc_7_dout;
-	7'b0000001: j1_io_din = crc_16_dout;
-
-        default: j1_io_din = 16'h0666;
+        12'b100000000000: j1_io_din = mult_dout; 
+        12'b010000000000: j1_io_din = div_dout;
+        12'b001000000000: j1_io_din = uart_dout; 
+        12'b000100000000: j1_io_din = dp_ram_dout; 
+        12'b000010000000: j1_io_din = spi_master_dout; 
+	12'b000001000000: j1_io_din = crc_7_dout;
+	12'b000000100000: j1_io_din = crc_16_dout;
+	12'b000000010000: j1_io_din = pwm_dout;
+    	12'b000000001000: j1_io_din = lcd_dout;
+	12'b000000000100: j1_io_din = posicion_dout;	
+	12'b000000000010: j1_io_din = timmer_dout;
+	12'b000000000001: j1_io_din = distancia_dout;	
+		
+		
+		default: j1_io_din = 16'h0666;
       endcase
   end
  // ============== MUX ========================  // 
